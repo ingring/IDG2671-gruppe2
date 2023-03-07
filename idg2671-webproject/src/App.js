3949312b6c624ff5da03bd1e40be2490a782f707
lisamarimyrene <lisamy@stud.ntnu.no>
2023-03-07 11:03:59 +0100

    tried added tailwind button in footer

--- idg2671-webproject/src/App.js
+++ idg2671-webproject/src/App.js
@@ -1,11 +1,13 @@
 // Husk å importere componente ditt her
 import logo from './logo.svg';
 import './App.css';
+import Footer from './components/Footer/Footer';
 
 function App() {
   return (
     // Her legger du til componentene du har laget
     <div className="App">
+      <Footer />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
---
+++ idg2671-webproject/src/components/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Footer() {
+    return (
+        <div>
+            <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
+                Button
+            </button>
+        </div>
+    )
+}
+export default Footer;
