30bc6deeb732ca6a6d16dedde1e91f92b1a7609b
lisamarimyrene <lisamy@stud.ntnu.no>
2023-03-07 11:19:04 +0100

    tried fixing tailwind

--- idg2671-webproject/src/components/Footer/Footer.jsx
+++ idg2671-webproject/src/components/Footer/Footer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Footer() {
     return (
         <div>
-            <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
+            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
                 Button
             </button>
         </div>
