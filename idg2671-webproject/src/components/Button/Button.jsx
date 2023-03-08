9846f4d03cb37cd4873a6a305637027efa235067
lisamarimyrene <lisamy@stud.ntnu.no>
2023-03-08 19:58:37 +0100

    downloaded react-router-dom and added routes

--- idg2671-webproject/src/components/Button/Button.jsx
+++ idg2671-webproject/src/components/Button/Button.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Button = (props) => {
-    const title = props.title;
+    const title = props.title; 
     return (
         <button className="w-28 h-8 bg-blue-primary text-white rounded-lg shadow-md">
             {title}
