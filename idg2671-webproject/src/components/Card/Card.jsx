a87cc0d3c724e30bd58cd7637871ccea115eab7b
lisamarimyrene <lisamy@stud.ntnu.no>
2023-03-08 19:25:55 +0100

    changed card component stylings

--- idg2671-webproject/src/components/Card/Card.jsx
+++ idg2671-webproject/src/components/Card/Card.jsx
@@ -2,8 +2,10 @@ import React from "react";
 
 function Card() {
     return (
-        <div className="bg-blue-lighter h-96 w-80">
-            <p>hei</p>
+        <div className="bg-blue-lighter h-96 w-80 rounded-xl shadow-md">
+            <h3 className="font-bold">Tool</h3>
+            <img className="rounded-xl" src="#" alt="#" />
+            <p>Course</p>
         </div>
     )
 }
