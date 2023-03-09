8191f2156bb2983012ec9debbf1a613dd7988c76
lisamarimyrene <lisamy@stud.ntnu.no>
2023-03-09 14:00:30 +0100

    Done is with card ish

--- idg2671-webproject/src/components/Card/Card.jsx
+++ idg2671-webproject/src/components/Card/Card.jsx
@@ -1,13 +1,14 @@
 
 import React from "react";
 import Button from '../Button/Button';
+import Image from '../../assets/img/3d-printer.jpeg'
 
 export default function Card() {
     return (
-        <div className="bg-blue-lighter h-96 w-80 rounded-xl shadow-md">
-            <h3 className="font-bold">Tool</h3>
-            <img className="rounded-xl" src="#" alt="#" />
-            <p>Course</p>
+        <div className="bg-blue-lighter h-auto w-2/12 rounded-xl shadow-md p-5">
+            <h3 className="font-bold pb-7">Tool</h3>
+            <img className="rounded-md mb-8" src={Image} alt="." />
+            <p className="pb-7">Course: HMS</p>
             <Button title="Book"/>
         </div>
     )
