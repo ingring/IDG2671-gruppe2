6f0ddbfa93a6d1fca718062adc8ddce8c87022db
lisamarimyrene <lisamy@stud.ntnu.no>
2023-03-09 09:43:33 +0100

    changed the card

--- idg2671-webproject/src/components/Button/Button.jsx
+++ idg2671-webproject/src/components/Button/Button.jsx
@@ -4,7 +4,7 @@ const Button = (props) => {
     const title = props.title; 
     return (
         <button className="w-28 h-8 bg-blue-primary text-white rounded-lg shadow-md">
-            {title}
+            {porops.title}
         </button>
     )
 }
