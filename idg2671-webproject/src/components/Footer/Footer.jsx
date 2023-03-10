49c4f52bfde16950ad623e6c83c69f5929cb66e1
lisamarimyrene <lisamy@stud.ntnu.no>
2023-03-10 12:50:51 +0100

    changed footer stylings

--- idg2671-webproject/src/components/Footer/Footer.jsx
+++ idg2671-webproject/src/components/Footer/Footer.jsx
@@ -3,21 +3,25 @@ import NtnuLogoSmallWhite from '../../assets/img/ntnu-logo-small-white.png'
 
 function Footer() {
     return (
-        <div className="bg-blue-darker text-white w-screen	h-40">
-            <img className="w-20" src={NtnuLogoSmallWhite} alt="NTNU logo small" />
-            <div className="">
-                <h2 className="font-bold">Contact</h2>
-                <p>email@something.no</p>
+        <footer className="bg-blue-darker text-white w-screen h-auto w-screen p-12 flex">
+            
+            <img className="w-20 flex inline" src={ NtnuLogoSmallWhite } alt="NTNU logo small" />
+
+            <div className="flex justify-evenly w-screen items-center">
+                <div>
+                    <h2 className="font-bold pb-6">Contact</h2>
+                    <p>email@something.no</p>
+                </div>
+                <div>
+                    <h2 className="font-bold pb-6">Address</h2>
+                    <p>Raufossvegen 40, 2821 Gjøvik</p>
+                </div>
+                <div>
+                    <h2 className="font-bold pb-6">Workshop Responsible</h2>
+                    <p>Ida Parelius</p>
+                </div>
             </div>
-            <div className="">
-                <h2 className="font-bold">Adress</h2>
-                <p>Raufossvegen 40, 2821 Gjøvik</p>
-            </div>
-            <div className="">
-                <h2 className="font-bold">Workshop Responsible</h2>
-                <p>Ida Parelius</p>
-            </div>
-        </div>
+        </footer>
     )
 }
 export default Footer;
