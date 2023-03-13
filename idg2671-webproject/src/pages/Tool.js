2c6ca813064689075b93f6c8bbd026d5a79c5d20
ingring <ingrid.gladheim@student.uib.no>
2023-03-13 19:24:46 +0100

    imported img from the card branch

---
+++ idg2671-webproject/src/pages/Tool.js
@@ -0,0 +1,32 @@
+import DisplayTool from "../components/DisplayTool/DisplayTool";
+import Image from '../assets/img/3d-printer.jpeg';
+
+// Parsing the data with JSON.parse since we get a long piece of text when receiving 
+// some JSON data from an API or from a file
+// let jsonData = JSON.parse(jsonString)
+
+let jsonData = {
+    "title":"Laserkutter",
+    "status":"OK",
+    "course":"HMS",
+    "imgSrc":Image,
+    "imgAlt":"",
+    "description":"Laserkutteren er et verkøty som kan gjøre mange ulike ting. Du kan for eksempel kutte ut ulike ting i ulike meterialer. Lorem ipsum, lorem lorem ipsum."
+}
+
+function DisplayToolPage() {
+    return (
+      <div className="App">
+        <DisplayTool 
+            title={jsonData["title"]}
+            status={jsonData["status"]}
+            course={jsonData["course"]} 
+            imgSrc={jsonData["imgSrc"]}
+            imgAlt={jsonData["imgAlt"]} 
+            description={jsonData["description"]}
+        />
+      </div>
+    );
+  }
+  
+ export default DisplayToolPage;
