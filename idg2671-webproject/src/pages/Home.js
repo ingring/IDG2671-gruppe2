ceec7402fa402c70da3b30db6c4302062cf1ba7a
lisamarimyrene <lisamy@stud.ntnu.no>
2023-03-14 23:22:14 +0100

    edited color

--- idg2671-webproject/src/pages/Home.js
+++ idg2671-webproject/src/pages/Home.js
@@ -1,6 +1,5 @@
 import Footer from '../components/Footer/Footer';
 import Button from '../components/Button/Button';
-import CalendarComponent from '../components/Calendar/Calendar';
 
 function HomePage() {
     return (
@@ -8,7 +7,6 @@ function HomePage() {
             <div className="bg-grey-lighter min-h-screen">
                 <h1 className="pt-4">Dette er hjemmesiden!</h1>
                 <Button title="Add tool" />
-                <CalendarComponent />
             </div>
             
             <Footer />
