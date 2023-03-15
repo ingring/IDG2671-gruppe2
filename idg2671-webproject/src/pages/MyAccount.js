b8e0831708fab82115659d65a9e820875fb85255
anoshc <anoshc@stud.ntnu.no>
2023-03-15 13:18:39 +0100

    added dummy data to my account

--- idg2671-webproject/src/pages/MyAccount.js
+++ idg2671-webproject/src/pages/MyAccount.js
@@ -1,11 +1,23 @@
 import MyAccount from "../components/Form/MyAccount";
 import Footer from "../components/Footer/Footer";
 
+let jsonData = {
+  name: "anosh",
+  email: "anoshc@stud.ntnu.no",
+  study: "web development",
+  department: "ntnu",
+};
+
 function MyAccountPage() {
   return (
     <>
       <div className="bg-grey-lighter min-h-screen">
-        <MyAccount />
+        <MyAccount
+          name={jsonData["name"]}
+          email={jsonData["email"]}
+          study={jsonData["study"]}
+          department={jsonData["department"]}
+        />
       </div>
       <Footer />
     </>
