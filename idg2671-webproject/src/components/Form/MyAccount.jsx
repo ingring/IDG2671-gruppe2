dcc12e7599afa52e890ea201b9d8a4387f44a645
anoshc <anoshc@stud.ntnu.no>
2023-03-15 13:06:38 +0100

    updated forms

--- idg2671-webproject/src/components/Form/MyAccount.jsx
+++ idg2671-webproject/src/components/Form/MyAccount.jsx
@@ -1,41 +1,31 @@
 import React from "react"
-import Button from '../Button/Button';
 
 export default function MyAccountPage() {
     return (
-
         
-<section>
-        <div class="flex items-center justify-center md:h-screen">
-           
-            <div class="w-full sm:max-w-md">
-                <div class="p-6 md:space-y-6 sm:p-8">
-                    <h1 class="font-bold text-gray-900 md:text-2xl">
-                        My Account
-                    </h1>
-                    <form class="md:space-y-6" action="#">
-                    <div>
-                        <label for="name" className="block mb-2">Name</label>
-                            <input type="text" name="name" id="name" class="border sm:text-sm rounded-lg focus:ring-primary-600 w-full p-2.5 dark:bg-grey-lighter" placeholder="Name" required=""></input>
-                        </div>
-                        <div>
-                            <label for="email" className="block mb-2">Email</label>
-                            <input type="email" name="email" id="email" class="border sm:text-sm rounded-lg focus:ring-primary-600 w-full p-2.5 dark:bg-grey-lighter" placeholder="name@company.com" required=""></input>
-                        </div>
-                        <div>
-                            <label for="study" className="block mb-2">Study</label>
-                            <input type="text" name="study" id="study" class="border sm:text-sm rounded-lg focus:ring-primary-600 w-full p-2.5 dark:bg-grey-lighter" placeholder="Study" required=""></input>
-                        </div>
-                        <div>
-                            <label for="department" className="block mb-2">Department</label>
-                            <input type="text" name="department" id="department" class="border sm:text-sm rounded-lg focus:ring-primary-600 w-full p-2.5 dark:bg-grey-lighter" placeholder="Department" required=""></input>
-                        </div>
-                        <Button title="Save"/>
-                    </form>
-                </div>
-            </div>
-        </div>
-      </section>
+<section className="flex items-center justify-center min-h-screen">     
+<dl>
+<h1 className="font-bold md:text-2xl text-left">
+     My Account
+ </h1>
+    <div className="flex-col pt-3">
+        <dt className="mb-1 md:text-lg font-semibold text-left">Name</dt>
+        <dd className="text-lg text-left">Anosh</dd>
+    </div>
+    <div className="flex-col pt-3">
+        <dt className="mb-1 md:text-lg font-semibold text-left">Email</dt>
+        <dd className="text-lg text-left">anoshc@stud.ntnu.no</dd>
+    </div>
+    <div className="flex-col pt-3">
+        <dt className="mb-1 md:text-lg font-semibold text-left">Study</dt>
+        <dd className="text-lg text-left">Web development</dd>
+    </div>
+    <div className="flex-col pt-3">
+        <dt className="mb-1 md:text-lg font-semibold text-left">Department</dt>
+        <dd className="text-lg text-left">Ntnu</dd>
+    </div>
+</dl>
+</section>
 
 
         
