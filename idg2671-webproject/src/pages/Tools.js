bfd40d36d6b50bc5c4fcb65fdc11649fbbea2bca
ingring <ingrid.gladheim@student.uib.no>
2023-03-16 16:46:09 +0100

    List component

--- idg2671-webproject/src/pages/Tools.js
+++ idg2671-webproject/src/pages/Tools.js
@@ -1,7 +1,8 @@
 import Card from '../components/Card/Card';
 import Footer from '../components/Footer/Footer';
 import Searchbar from '../components/Searchbar/Searchbar';
-import Filter from '../components/Filter/Filter'
+import Filter from '../components/Filter/Filter';
+import List from '../components/List/List'
 
 function ToolPage() {
     return (
@@ -10,7 +11,10 @@ function ToolPage() {
           <Card />
 
           <Searchbar />
+          <List />
+          <br /><br />
           <Filter />
+          <List />
         </div>
         <Footer />
       </>
