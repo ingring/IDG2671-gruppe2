1dec3a710f6900395b2bfeac6dfe40d6cebbd884
lisamarimyrene <lisamy@stud.ntnu.no>
2023-03-17 14:04:55 +0100

    created calendar component with functionality

--- idg2671-webproject/src/components/Calendar/Calendar.jsx
+++ idg2671-webproject/src/components/Calendar/Calendar.jsx
@@ -1,89 +1,40 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { set } from "lodash";
 
-function Calendar() {
-  return (
-    <div className="calendar-container bg-white flex flex-col max-w-lg p-10 rounded-md">
-      <p className="flex justify-start mb-5">Velg dato</p>
+import buildCalendar from "./BuildCalendar";
+import dayStyles, { beforeToday } from "./CalendarStyles";
+import CalendarHeader from "./CalendarHeader";
 
-      <div className="calendar-header flex flex-row justify-between mb-2">
-        <button className="leftBtn my-6 w-3">
-          <svg viewBox="0 0 63 71" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path d="M5.75 33.3349L54.5 5.18912C56.1667 4.22686 58.25 5.4297 58.25 7.35418V63.6458C58.25 65.5703 56.1666 66.7731 54.5 65.8109L5.75002 37.6651C4.08333 36.7028 4.08335 34.2972 5.75 33.3349Z" stroke="#636363" stroke-width="9" />
-          </svg>
-        </button>
-        <h2 className="monthText my-6 font-bold">Februar 2023</h2>
-        <button className="leftBtn my-6 w-3 rotate-180">
-          <svg viewBox="0 0 63 71" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path d="M5.75 33.3349L54.5 5.18912C56.1667 4.22686 58.25 5.4297 58.25 7.35418V63.6458C58.25 65.5703 56.1666 66.7731 54.5 65.8109L5.75002 37.6651C4.08333 36.7028 4.08335 34.2972 5.75 33.3349Z" stroke="#636363" stroke-width="9" />
-          </svg>
-        </button>
-      </div>
+import "./Calendar.css";
 
-      <div className="calendarTable">
-        <div className="calendarHeader mb-2">
-          <ul className="flex flex-row justify-evenly ">
-            <li className="w-12">Mon</li>
-            <li className="w-12">Thues</li>
-            <li className="w-12">Wed</li>
-            <li className="w-12">Thurs</li>
-            <li className="w-12">Fri</li>
-            <li className="w-12">Sat</li>
-            <li className="w-12">Sun</li>
-          </ul>
+// Kilde: https://www.youtube.com/watch?v=5jRrVqRWqsM
+export default function Calendar2({ value, onChange }) {
+    // State
+    const [calendar, setCalendar] = useState([]);
+
+    useEffect(() => {
+        setCalendar(buildCalendar(value));
+    }, [value]);
+
+    return (
+        <div className="calendar">
+            <CalendarHeader value={value} setValue={onChange} />
+            <div className="calendarBody">
+                <div className="day-names">
+                    {["Mon", "Thue", "Wed", "Thur", "Fri", "Sat", "Sun"].map((d) => (
+                        <div className="week">{d}</div>
+                    ))}
+                </div>
+                {calendar.map((week) => (
+                    <div>
+                        {week.map((day) => (
+                            <div className="day" onClick={() => !beforeToday(day) && onChange(day)}>
+                                <div className={dayStyles(day, value)}>{day.format("D").toString()}</div>
+                            </div>
+                        ))}
+                    </div>
+                ))}
+            </div>
         </div>
-
-        <div className="calendarCells">
-
-          <ol className="flex flex-row justify-evenly mb-1">
-            <li><button className="w-14 p-2 bg-green">1</button></li>
-            <li><button className="w-14 p-2 bg-green">2</button></li>
-            <li><button className="w-14 p-2 bg-green">3</button></li>
-            <li><button className="w-14 p-2 bg-green">4</button></li>
-            <li><button className="w-14 p-2 bg-green">5</button></li>
-            <li><button className="w-14 p-2 bg-green">6</button></li>
-            <li><button className="w-14 p-2 bg-green">7</button></li>
-          </ol>
-          <ol className="flex flex-row justify-evenly mb-1">
-            <li><button className="w-14 p-2 bg-green">1</button></li>
-            <li><button className="w-14 p-2 bg-green">2</button></li>
-            <li><button className="w-14 p-2 bg-green">3</button></li>
-            <li><button className="w-14 p-2 bg-green">4</button></li>
-            <li><button className="w-14 p-2 bg-green">5</button></li>
-            <li><button className="w-14 p-2 bg-green">6</button></li>
-            <li><button className="w-14 p-2 bg-green">7</button></li>
-          </ol>
-          <ol className="flex flex-row justify-evenly mb-1">
-            <li><button className="w-14 p-2 bg-green">1</button></li>
-            <li><button className="w-14 p-2 bg-green">2</button></li>
-            <li><button className="w-14 p-2 bg-green">3</button></li>
-            <li><button className="w-14 p-2 bg-green">4</button></li>
-            <li><button className="w-14 p-2 bg-green">5</button></li>
-            <li><button className="w-14 p-2 bg-green">6</button></li>
-            <li><button className="w-14 p-2 bg-green">7</button></li>
-          </ol>
-          <ol className="flex flex-row justify-evenly mb-1">
-            <li><button className="w-14 p-2 bg-green">1</button></li>
-            <li><button className="w-14 p-2 bg-green">2</button></li>
-            <li><button className="w-14 p-2 bg-green">3</button></li>
-            <li><button className="w-14 p-2 bg-green">4</button></li>
-            <li><button className="w-14 p-2 bg-green">5</button></li>
-            <li><button className="w-14 p-2 bg-green">6</button></li>
-            <li><button className="w-14 p-2 bg-green">7</button></li>
-          </ol>
-          <ol className="flex flex-row justify-evenly">
-            <li><button className="w-14 p-2 bg-green">1</button></li>
-            <li><button className="w-14 p-2 bg-green">2</button></li>
-            <li><button className="w-14 p-2 bg-green">3</button></li>
-            <li><button className="w-14 p-2 bg-green">4</button></li>
-            <li><button className="w-14 p-2 bg-green">5</button></li>
-            <li><button className="w-14 p-2 bg-green">6</button></li>
-            <li><button className="w-14 p-2 bg-green">7</button></li>
-          </ol>
-        </div>
-
-      </div>
-    </div>
-  )
-}
-
-export default Calendar;
+    )
+}   
