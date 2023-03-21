6cd43c9f455d0afb017da16141acb4ba0ca346c9
lisamarimyrene <lisamy@stud.ntnu.no>
2023-03-21 10:28:36 +0100

    added type prop to button

--- idg2671-webproject/src/components/BookTool/BookTool.jsx
+++ idg2671-webproject/src/components/BookTool/BookTool.jsx
@@ -5,25 +5,26 @@ export default function BookTool(props) {
     return (  
         
         <div className="BookTool w-9/12 flex justify-center flex-col">
-            <h1 className="md:text-3xl mb-6">Book tool</h1>
-        
-            <h3 className="font-semibold mb-1">Tool</h3>
-            <p className="mb-6">{props.tool}</p>
-
-            <h3 className="font-semibold mb-1">Date</h3>
-            <p className="mb-6">{props.date}</p>
+            <form action="POST">
+                <h1 className="md:text-3xl mb-6">Book tool</h1>
             
-            <h3 className="font-semibold mb-1">Time</h3>
-            <p className="mb-6">{props.time}</p>
+                <h3 className="font-semibold mb-1">Tool</h3>
+                <p className="mb-6">{props.tool}</p>
 
-            <h3 className="font-semibold mb-1">Duration</h3>
-            <p className="mb-6">{props.duration}</p>
+                <h3 className="font-semibold mb-1">Date</h3>
+                <p className="mb-6">{props.date}</p>
+                
+                <h3 className="font-semibold mb-1">Time</h3>
+                <p className="mb-6">{props.time}</p>
 
-            <h3 className="font-semibold mb-1">Description</h3>
-            <input className="mb-6" type="text" name="" id="" />
+                <h3 className="font-semibold mb-1">Duration</h3>
+                <p className="mb-6">{props.duration}</p>
 
-            <Button title="Confirm"/>
+                <h3 className="font-semibold mb-1">Description</h3>
+                <input className="mb-6" type="text" name="" id="" />
 
+                <Button type="submit" title="Confirm"/>
+            </form>
         </div>
     )
 }
--- idg2671-webproject/src/components/Button/Button.jsx
+++ idg2671-webproject/src/components/Button/Button.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 
-const Button = (props) => {
+const Button = (type="", title) => {
+
     return (
-        <button className="w-24 h-9 bg-blue-darker text-white hover:bg-blue-primary rounded-2xl shadow-md">
-            {props.title}
+        <button type={type} className="w-24 h-9 bg-blue-darker text-white hover:bg-blue-primary rounded-2xl shadow-md">
+            {title}
         </button>
     )
 }
