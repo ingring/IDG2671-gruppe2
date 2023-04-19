4828cd399952fa3f59deae96747a40723fee109d
lisamarimyrene <lisamy@stud.ntnu.no>
2023-04-19 22:43:58 +0200

    added infoboxes

--- idg2671-webproject/src/pages/AdminAllBookings.js
+++ idg2671-webproject/src/pages/AdminAllBookings.js
@@ -5,12 +5,15 @@ import ListBookedBy from "../components/List/ListBookedBy";
 function AdminAllBookings() {
   return (
     <div className="bg-grey-lighter min-h-screen">
+      <div className="bg-blue-lighter flex flex-col items-center w-full">
+        <div className="w-3/5 pt-16 pb-12 flex justify-left flex-col">
+          <h1 className="text-3xl pb-8">Admin page</h1>
+          <p className="w-3/5">Here you can get an overview of all the active bookings, as well get an overview of all the tools and users in the workshop. On the different sub pages, you can also add, edit or delete tools and users.</p>        </div>
+      </div>
       <AdminSubnav />
-      <div className="flex flex-col items-center justify-center p-20">
-      <h1 className="text-3xl pb-8">All active bookings</h1>
-        <p className="w-2/5 text-center">Here you have an overview of all active bookings at Verkstedet. You can also see who has booked the tool.</p>
+      <div className="flex flex-col items-center justify-center">
         <div className="mt-20 w-3/4">
-        <h2 className="text-2xl mb-6">Active bookings</h2>
+          <h2 className="text-2xl mb-6">Active bookings</h2>
           <Filter />
           <ListBookedBy />
         </div>
