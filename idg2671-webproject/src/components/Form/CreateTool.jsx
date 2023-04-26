99bb9345650f715b823ab452dc609e2c4779f120
lisamarimyrene <lisamy@stud.ntnu.no>
2023-04-26 17:34:13 +0200

    working on the image uploader

--- idg2671-webproject/src/components/Form/CreateTool.jsx
+++ idg2671-webproject/src/components/Form/CreateTool.jsx
@@ -1,7 +1,48 @@
 import React from "react";
+import { useState } from "react";
+import axios from "axios";
 import InputButton from '../Button/InputButton';
+import e from "express";
 
 export default function CreateTool(){
+
+    const [file, setFile] = useState("");
+    const [image, setImage] = useState("");
+
+    // https://developer.mozilla.org/en-US/docs/Web/API/FileReader
+    // File reader to turn it into an readable url
+    function previewFiles(file) {
+        const reader = new FileReader();
+        reader.readAsDataURL(file);
+
+        // Show image result
+        reader.onloadend = () => {
+            console.log('Reader result: ' + reader.result);
+            setImage(reader.result);
+        }
+    }
+
+    // Handle that target the uploaded file
+    const handleChange = (e) => {
+        const file = e.target.files[0]; // Get the 0 index of the file, which is the image
+        console.log(e.target.files[0]);
+        setFile(file);
+        previewFiles(file); // Enables you to see your uploaded file
+    }
+    const handleSubmit = async () => {
+        e.preventDefault();
+        const result = await axios.post("http://localhost:3000/api/image", {
+            image: image
+        })
+        try {
+            console.log(result.data);
+        } catch(err) {
+            console.log(err);
+        }
+        // console.log(e.target.files);
+        // setFile(e.target.files)
+    }
+
     return(
         <section>
         <div className="flex items-center justify-center">
@@ -10,18 +51,18 @@ export default function CreateTool(){
                     <h1 className="md:text-2xl text-left">
                         Create / Modify tool
                     </h1>
-                    <form className="md:space-y-6 flex justify-start flex-col pb-3" action="#">
+                    <form onSubmit={e => handleSubmit(e)} className="md:space-y-6 flex justify-start flex-col pb-3">
                         <div>
                             <label for="title" className="block mb-2 text-left lg:text-lg">Title</label>
-                            <input type="text" name="title" id="title" className="text-left border border-grey-mediumLight lg:text-lg rounded-lg w-full dark:bg-grey-lighter" required=""></input>
+                            <input type="text" name="title" id="title" className="text-left border border-grey-mediumLight lg:text-lg rounded-lg w-full dark:bg-grey-lighter" required></input>
                         </div>
                         <div>
                             <label for="description" className="block mb-2 text-left lg:text-lg">Description</label>
-                            <input type="text" name="description" id="description" className="text-left border border-grey-mediumLight lg:text-lg rounded-lg w-full h-24 dark:bg-grey-lighter" required=""></input>
+                            <input type="text" name="description" id="description" className="text-left border border-grey-mediumLight lg:text-lg rounded-lg w-full h-24 dark:bg-grey-lighter" required></input>
                         </div>
                         <div>
                             <label for="course" className="block mb-2 text-left lg:text-lg">Safety course</label>
-                            <select type="text" name="course" id="course" className="text-left border border-grey-mediumLight lg:text-lg rounded-lg w-full dark:bg-grey-lighter" required="">
+                            <select type="text" name="course" id="course" className="text-left border border-grey-mediumLight lg:text-lg rounded-lg w-full dark:bg-grey-lighter" required>
                                 <option value=""></option>
                                 <option value="hms">HMS</option>
                                 <option value="none">None</option>
@@ -29,18 +70,23 @@ export default function CreateTool(){
                         </div>
                         <div className="pb-6">
                             <label for="status" className="block mb-2 text-left lg:text-lg">Status</label>
-                            <select type="text" name="status" id="status" className="text-left border border-grey-mediumLight lg:text-lg rounded-lg w-full dark:bg-grey-lighter" required="">
+                            <select type="text" name="status" id="status" className="text-left border border-grey-mediumLight lg:text-lg rounded-lg w-full dark:bg-grey-lighter" required>
                                 <option value=""></option>
                                 <option value="ok">OK</option>
                                 <option value="broken">Broken</option>
                                 <option value="underConstruction">Under construction</option>
                             </select>
                         </div>
+                        <div>
+                            <label htmlFor="fileInput" className="block mb-2 text-left lg:text-lg">Upload image</label>
+                            <input type="file" name="image" id="fileInput" onChange={e => handleChange(e)} accept="image/png, image/jpeg, image/jpg, image/svg" className="text-left border border-grey-mediumLight lg:text-lg rounded-lg w-full h-24 dark:bg-grey-lighter" required></input>
+                        </div>
                         <InputButton value="Submit" />
                     </form>
                 </div>
             </div>
         </div>
+        <img src={image} alt="" />
       </section>
     )
 }
