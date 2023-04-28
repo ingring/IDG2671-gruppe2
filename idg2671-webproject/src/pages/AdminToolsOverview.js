8cb889a8e1a9c5791eb55f650c3ab1cb6fff374b
lisamarimyrene <lisamy@stud.ntnu.no>
2023-04-28 13:16:37 +0200

    fixed cards

--- idg2671-webproject/src/pages/AdminToolsOverview.js
+++ idg2671-webproject/src/pages/AdminToolsOverview.js
@@ -9,7 +9,7 @@ function AdminToolsOverview() {
       <div className="bg-blue-lighter flex flex-col items-center w-full">
         <div className="w-3/5 pt-16 pb-12 flex justify-left flex-col">
           <h1 className="text-3xl pb-8">Admin page</h1>
-          <p className="w-3/5">Here you can get an overview of all the active bookings, as well get an overview of all the tools and users in the workshop. On the different sub pages, you can also add, edit or delete tools and users.</p>        </div>
+          <p className="pb-6 md:w-3/5">Here you can get an overview of all the active bookings, as well get an overview of all the tools and users in the workshop. On the different sub pages, you can also add, edit or delete tools and users.</p>        </div>
       </div>
       <AdminSubnav />
       <div className="flex flex-col items-center justify-center">
--- idg2671-webproject/src/pages/MyAccountRequestTools.js
+++ idg2671-webproject/src/pages/MyAccountRequestTools.js
@@ -7,7 +7,7 @@ function MyAccountRequestTool() {
       <div className="bg-blue-lighter flex flex-col items-center w-full">
         <div className="w-3/5 pt-16 pb-14 flex justify-left flex-col">
         <h1 className="text-3xl pb-8">My account</h1>
-          <p className="w-3/5">Here you can get an overview of your account with your credentials, as well as your previous and active bookings. You can also leave a request for new tools if needed.</p>
+          <p className="pb-6 md:w-3/5">Here you can get an overview of your account with your credentials, as well as your previous and active bookings. You can also leave a request for new tools if needed.</p>
         </div>
       </div>
       <MyAccountSubnav />
