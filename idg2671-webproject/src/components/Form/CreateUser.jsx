989c29b2b293a7156c7560a215342af14913594a
ingring <ingrid.gladheim@student.uib.no>
2023-04-29 12:47:14 +0200

    admin forms - createTool, createUser. My account - request tool

--- idg2671-webproject/src/components/Form/CreateUser.jsx
+++ idg2671-webproject/src/components/Form/CreateUser.jsx
@@ -1,33 +1,78 @@
-import React from "react";
+import React, { useState } from "react";
+import useAxiosPrivate from "../../axios/useAxiosPrivate";
 import InputButton from '../Button/InputButton';
 
-export default function CreateUser() {
+export default function CreateUser({url}) {
+    const [formData, setFormData] = useState({
+        firstname: "",
+        lastname: "",
+        username: "",
+        email: "",
+        field_of_study: "",
+      });
+
+      const axiosPrivate = useAxiosPrivate();
+    
+      const handleSubmit = async (event) => {
+        event.preventDefault();
+    
+        try {
+          const response = await axiosPrivate.post(`api/users`, 
+          {
+            firstName: formData.firstName,
+            lastName: formData.lastName,
+            username: formData.username,
+            email: formData.email,
+            fieldOfStudy: formData.fieldOfStudy
+        });
+          console.log(response.data);
+        } catch (error) {
+          console.error(error);
+        }
+      };
+    
+      const handleChange = (event) => {
+        setFormData({
+          ...formData,
+          [event.target.name]: event.target.value,
+        });
+      };
+
     return (
         <div className="flex items-center justify-center">
             <div className="w-full">
                 <h1 className="text-xl md:text-2xl text-left mb-10">
                     Create / Modify user
                 </h1>
-                <form className="md:space-y-6 flex justify-start flex-col pb-3" action="#">
+                <form className="md:space-y-6 flex justify-start flex-col pb-3" onSubmit={handleSubmit}>
                 <div className="mb-6 md:mb-0">
                         <label for="firstname" className="block mb-2 text-left">First name</label>
-                        <input type="text" name="firstname" id="firstname" className="text-left border-grey-mediumLight p-2 h-9 rounded-md w-full" required></input>
+                        <input type="text" name="firstname" id="firstname" className="text-left border-grey-mediumLight p-2 h-9 rounded-md w-full" 
+                            value={formData.firstname} onChange={handleChange} required></input>
                     </div>
                     <div className="mb-6 md:mb-0">
                         <label for="lastname" className="block mb-2 text-left">Last name</label>
-                        <input type="text" name="lastname" id="lastname" className="text-left border-grey-mediumLight p-2 h-9 rounded-md w-full" required></input>
+                        <input type="text" name="lastname" id="lastname" 
+                            className="text-left border-grey-mediumLight p-2 h-9 rounded-md w-full" 
+                            value={formData.lastname} onChange={handleChange} required></input>
                     </div>
                     <div className="mb-6 md:mb-0">
                         <label for="username" className="block mb-2 text-left">Username</label>
-                        <input type="text" name="username" id="username" className="text-left border-grey-mediumLight p-2 h-9 rounded-md w-full" required></input>
+                        <input type="text" name="username" id="username" 
+                            className="text-left border-grey-mediumLight p-2 h-9 rounded-md w-full" 
+                            value={formData.username} onChange={handleChange} required></input>
                     </div>
                     <div className="mb-6 md:mb-0">
                         <label for="email" className="block mb-2 text-left">Email</label>
-                        <input type="text" name="email" id="email" className="text-left border-grey-mediumLight p-2 h-9 rounded-md w-full" required></input>
+                        <input type="text" name="email" id="email" 
+                            className="text-left border-grey-mediumLight p-2 h-9 rounded-md w-full" 
+                            value={formData.email} onChange={handleChange} required></input>
                     </div>
                     <div className="pb-14 md:pb-6">
                         <label for="field_of_study" className="block mb-2 text-left">Field of study</label>
-                        <select type="text" name="field_of_study" id="field_of_study" className="text-left border-grey-mediumLight p-2 h-9 rounded-md w-full" required>
+                        <select type="text" name="field_of_study" id="field_of_study" 
+                            className="text-left border-grey-mediumLight p-2 h-9 rounded-md w-full" 
+                            value={formData.field_of_study} onChange={handleChange} required>
                             <option value=""></option>
                             <option value="BWU">BWU</option>
                             <option value="BIXD">BIXD</option>
--- idg2671-webproject/src/components/Form/RequestTool.jsx
+++ idg2671-webproject/src/components/Form/RequestTool.jsx
@@ -5,48 +5,48 @@ import AuthContext from "../../context/AuthProvider";
 import useAxiosPrivate from "../../axios/useAxiosPrivate";
 
 export default function RequestTool(){
-    const { auth } = useContext(AuthContext);
-    const username = auth.username
+    const [formData, setFormData] = useState({
+        toolname: "",
+        description: "",
+      });
 
-    const [data, setData] = useState([]);
-  
-    const axiosPrivate = useAxiosPrivate();
-  
-    useEffect(() => {
-        let isMounted = true
-        const controller = new AbortController()
-        const getUser = async () => {
-            try {
-                const response = await axiosPrivate.post(`api/tool`, {
-                    signal: controller.signal
-                })
-                isMounted && setData(response.data)
-            }catch(err){
-                console.log(err)
-            }
+      const axiosPrivate = useAxiosPrivate();
+    
+      const handleSubmit = async (event) => {
+        event.preventDefault();
+    
+        try {
+          const response = await axiosPrivate.post(`api/tool`, 
+          {
+            tool: formData.toolname,
+            description: formData.description
+        });
+          console.log(response.data);
+        } catch (error) {
+          console.error(error);
         }
-
-        getUser()
-
-        if (!data) {
-            return <p>Loading...</p>;
-        }
-
-        return () => {
-            isMounted = false
-            controller.abort()
-        }
-    }, [data, username, axiosPrivate]);
+      };
+    
+      const handleChange = (event) => {
+        setFormData({
+          ...formData,
+          [event.target.name]: event.target.value,
+        });
+      };
     return(
         <div className="flex items-center justify-center w-full">
-            <form className="flex justify-start flex-col pb-6 w-3/5 md:w-1/5 lg:w-1/6">
+            <form className="flex justify-start flex-col pb-6 w-3/5 md:w-1/5 lg:w-1/6" onSubmit={handleSubmit}>
                 <div className="pb-6">
                     <label for="toolname" className="block mb-2 text-left">Name of tool</label>
-                    <input type="text" name="toolname" id="toolname" className="text-left border-grey-mediumLight p-2 h-8 rounded-md w-full" required></input>
+                    <input type="text" name="toolname" id="toolname" 
+                        className="text-left border-grey-mediumLight p-2 h-8 rounded-md w-full" 
+                        value={formData.toolname} onChange={handleChange} required></input>
                 </div>
                 <div className="pb-6">
                     <label for="description" className="block mb-2 text-left">Description</label>
-                    <input type="text" name="description" id="description" placeholder="Usage..." className="text-left p-2 border-grey-mediumLight rounded-md w-full pb-20" required></input>
+                    <input type="text" name="description" id="description" placeholder="Usage..." 
+                        className="text-left p-2 border-grey-mediumLight rounded-md w-full pb-20" 
+                        value={formData.description} onChange={handleChange} required></input>
                 </div>
                 <InputButton value="Request tool" />
             </form>
