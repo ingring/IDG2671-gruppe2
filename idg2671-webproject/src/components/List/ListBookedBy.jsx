1f179a08f1378737cdb6612be0624cb1967617d5
ingring <ingrid.gladheim@student.uib.no>
2023-04-29 18:28:10 +0200

    list tools admin

--- idg2671-webproject/src/components/List/ListBookedBy.jsx
+++ idg2671-webproject/src/components/List/ListBookedBy.jsx
@@ -1,74 +1,67 @@
-import React, { useState, useEffect, useContext } from "react";
+import React, { useState, useEffect } from "react";
 import "flowbite";
 import List from "./List";
 import ListElement from "./ListElement";
-import AuthContext from "../../context/AuthProvider";
 import useAxiosPrivate from "../../axios/useAxiosPrivate";
 
-function ListElementBookedBy({tool, date, time, username}) {
-    return (
-        <a href='./tools/id' className="flex">
-            <span className="w-1/4 font-semibold px-4">{tool}</span>
-            <span className="w-1/4 px-4">{date}</span>
-            <span className="w-1/4 px-4">{time}</span>
-            <span className="w-1/4 px-4">{username}</span>
-        </a>
-    )
+function ListElementBookedBy({ tool, date, time, username }) {
+  return (
+    <a href={`./tools/${tool}`} className="flex">
+      <span className="w-1/4 font-semibold px-4">{tool}</span>
+      <span className="w-1/4 px-4">{date}</span>
+      <span className="w-1/4 px-4">{time}</span>
+      <span className="w-1/4 px-4">{username}</span>
+    </a>
+  );
 }
 
 function ListBookedBy() {
+  const [user, setUser] = useState("");
+  const [data, setData] = useState([]);
 
-    const [user, setUser] = useState('')
-    const [data, setData] = useState([]);
-  
-    const axiosPrivate = useAxiosPrivate();
-  
-    useEffect(() => {
-        let isMounted = true
-        const controller = new AbortController()
-        const getUser = async () => {
-            try {
-                const response = await axiosPrivate.get(`api/users`, {
-                    signal: controller.signal
-                })
-                console.log('bookings: ', data)
-                console.log('username: ', user)
-                isMounted && setData(response.data.bookings) && setUser(response.data.username)
-            }catch(err){
-                console.log(err)
-            }
-        }
+  const axiosPrivate = useAxiosPrivate();
 
-        getUser()
+  useEffect(() => {
+    let isMounted = true;
+    const controller = new AbortController();
+    const fetchData = async () => {
+      try {
+        const response = await axiosPrivate.get(`api/users`, {
+          signal: controller.signal,
+        });
+        setData(response.data.bookings);
+        setUser(response.data.username);
+      } catch (err) {
+        console.log(err);
+      }
+    };
 
-        // if (!data) {
-        //     return <p>Loading...</p>;
-        // }
+    fetchData();
 
-        return () => {
-            isMounted = false
-            controller.abort()
-        }
-    }, [data, user, axiosPrivate]);
+    return () => {
+      isMounted = false;
+      controller.abort();
+    };
+  }, [axiosPrivate]);
 
-    return (         
-        <List>
-        {data ? (
+  return (
+    <List>
+      {data.length ? (
         data.map((booking) => (
-            <ListElement>
+          <ListElement key={booking._id}>
             <ListElementBookedBy
-                tool={booking.name}
-                date={booking.date}
-                time={booking.start_time}
-                username={user}
+              tool={booking.tool}
+              date={booking.date}
+              time={booking.start_time}
+              username={user}
             />
-            </ListElement>
+          </ListElement>
         ))
-        ) : (
+      ) : (
         <p>No current bookings</p>
-        )}
-        </List>
-    )
+      )}
+    </List>
+  );
 }
 
-export default ListBookedBy
+export default ListBookedBy;
