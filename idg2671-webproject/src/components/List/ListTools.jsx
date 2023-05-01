0e350fe7a138ab0e2da5609a41e8b7b7dcf4854c
anoshc <anoshc@stud.ntnu.no>
2023-05-01 12:38:07 +0200

    made the popup

--- idg2671-webproject/src/components/List/ListTools.jsx
+++ idg2671-webproject/src/components/List/ListTools.jsx
@@ -16,12 +16,13 @@ function ListElementTool({ tool }) {
 
 function ListTools() {
   const [data, setData] = useState(null);
-  const [showPopup, setShowPopup] = useState(false);
 
   useEffect(() => {
     async function fetchData() {
       const url = "tools";
       const response = await getAPI(url);
+      response.map(r => r.showPopup = false)
+      console.log(response);
       setData(response);
     }
 
@@ -32,9 +33,14 @@ function ListTools() {
     return <p>Loading...</p>;
   }
 
-  const togglePopup = () => {
-    setShowPopup(!showPopup);
+const togglePopup = (clickedTool) => {
+    setData(prevData =>
+      prevData.map(tool =>
+        tool === clickedTool ? { ...tool, showPopup: !tool.showPopup } : tool
+      )
+    );
   };
+  
 
   return (
     <List>
@@ -42,14 +48,8 @@ function ListTools() {
         <ListElement key={index}>
           <div className="flex flex-row justify-between items-center">
             <ListElementTool tool={tool.name} />
-            <button onClick={togglePopup}>
-              <svg
-                width="20"
-                height="20"
-                viewBox="0 0 26 26"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
+            <button onClick={() => togglePopup(tool)}>
+              <svg width="20" height="20" viewBox="0 0 26 26" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <path
                   d="M13 0C5.82075 0 0 5.82075 0 13C0 20.1793 5.82075 26 13 26C20.1793 26 26 20.1793 26 13C26 5.82075 20.1793 0 13 0ZM14.0833 19.5H11.9167V11.9167H14.0833V19.5ZM13 9.20833C12.103 9.20833 11.375 8.48033 11.375 7.58333C11.375 6.68633 12.103 5.95833 13 5.95833C13.897 5.95833 14.625 6.68633 14.625 7.58333C14.625 8.48033 13.897 9.20833 13 9.20833Z"
                   fill="#204F99"
@@ -57,13 +57,15 @@ function ListTools() {
               </svg>
             </button>
           </div>
-        </ListElement>
-      ))}
-      {showPopup && (
-        <div className="popup">
-          <p>Pop-up text</p>
+           {tool.showPopup && (
+        <div className=" items-center">
+          <div className="bg-white p-4 shadow-lg">
+            <p>{tool.description}</p>
+          </div>
         </div>
       )}
+        </ListElement>
+      ))}
     </List>
   );
 }
