0dfdb593a334a4d4b2591988f90224385a3120ce
anoshc <anoshc@stud.ntnu.no>
2023-05-02 10:21:00 +0200

    logout alert

--- idg2671-webproject/src/components/Logout/logout.jsx
+++ idg2671-webproject/src/components/Logout/logout.jsx
@@ -3,23 +3,32 @@ import { useNavigate } from 'react-router-dom';
 import { axiosPrivate } from '../../axios/axios';
 
 const logoutURL = 'api/logout';
+let loggedOut = false
 
-//logs out the user by sending a logout request to the backend which empties the cookie and deletes the refreshtoken in the database and if succesful redirects to login
 function Logout() {
-    console.log('testing from logout')
   const navigate = useNavigate();
+
   useEffect(() => {
     const doLogout = async () => {
-      const response = await axiosPrivate.get(logoutURL);
-      console.log(response.status);
+      if(!loggedOut) {
+        var confirmLogout = window.confirm('Are you sure you want to logout?');
+        loggedOut = true
+      }
+      // Display an alert to confirm logout
       
-      if (response.status === 200) {
-        // Redirect to another page
-        navigate('/Login');
-        //refreshes the window to show that the user is logged out
-        window.location.reload()
+      if (confirmLogout) {
+        const response = await axiosPrivate.get(logoutURL);
+        console.log(response.status);
+        
+        if (response.status === 200) {
+          // Redirect to another page
+          navigate('/Login');
+          // Refresh the window to show that the user is logged out
+          window.location.reload();
+        }
       }
     };
+
     doLogout();
   }, [navigate]);
 
