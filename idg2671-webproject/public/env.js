51edd37c8b4eab4560a571de77dd55fdba5f9d9f
ingring <ingrid.gladheim@student.uib.no>
2023-10-22 19:43:16 +0200

    added environment variable for the api link

---
+++ idg2671-webproject/public/env.js
@@ -0,0 +1,3 @@
+window.env = {
+  "API_URL": "https://idg2671-webproject-api.onrender.com"
+};
--- idg2671-webproject/src/axios/axios.js
+++ idg2671-webproject/src/axios/axios.js
@@ -1,8 +1,9 @@
 import axios from "axios";
-
+import env from "react-dotenv";
 
 //const BASE_URL = "https://webproject-api-production.up.railway.app/"
-const BASE_URL = "http://localhost:3001/"
+//const BASE_URL = "http://localhost:3001/"
+const BASE_URL = env.API_URL
 
 export default axios.create({
   baseURL: BASE_URL,
